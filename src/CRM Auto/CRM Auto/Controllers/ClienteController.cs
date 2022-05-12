10ba28a4025a21d09ff38e0b4b9fb29a360d4821
Ciro Watanabe <cw.artiga@gmail.com>
2022-05-12 12:36:53 -0300

    login cliente

---
+++ src/CRM Auto/CRM Auto/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using CRM_Auto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_Auto.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ValidarLogin(UsuarioModel usuario)
+        {
+            bool login = usuario.ValidarLogin();
+            if (login)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("LoginCliente");
+        }
+    }
+}
