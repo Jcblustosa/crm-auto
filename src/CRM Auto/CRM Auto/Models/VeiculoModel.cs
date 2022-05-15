ee605ab312280b312c957b4f33977195129b95ba
Ciro Watanabe <cw.artiga@gmail.com>
2022-05-15 18:39:39 -0300

    Cadastro de Veiculo

---
+++ src/CRM Auto/CRM Auto/Models/VeiculoModel.cs
@@ -0,0 +1,28 @@
+﻿using CRM_Auto.Util;
+
+namespace CRM_Auto.Models
+{
+    public class VeiculoModel
+    {
+
+        // Tabela Modelo_Carro
+
+        public string Nome_Modelo{ get; set; }
+
+        // Tabela Veiculo
+
+        public int Id_Veiculo_Cliente { get; set; }
+        public int Id_Modelo { get; set; }
+        public string Motorizacao { get; set; }
+        public string Ano_Fabricacao { get; set; }
+        public string Ano_Modelo { get; set; }
+
+        public void CadastroVeiculo()
+        {
+            string command = $"INSERT INTO VEICULO VALUES ('{Id_Modelo}', '{Motorizacao}', '{Ano_Fabricacao}', '{Ano_Modelo}')";
+
+            DAL dal = new DAL();
+            dal.InsertData(command);
+        }
+    }
+}
