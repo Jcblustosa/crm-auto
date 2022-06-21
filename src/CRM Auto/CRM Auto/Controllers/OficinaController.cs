861ca79e65fd6303c4fd7620f75ed683eef75c5c
Julio Lustosa <jcblustosa@sga.pucminas.br>
2022-06-21 16:56:46 -0300

    Corrigindo conflitos

--- src/CRM Auto/CRM Auto/Controllers/OficinaController.cs
+++ src/CRM Auto/CRM Auto/Controllers/OficinaController.cs
@@ -97,7 +97,6 @@ namespace CRM_Auto.Controllers
             return RedirectToAction("Sucesso");
         }
 
-<<<<<<< HEAD
         public IActionResult BuscarOficinas()
         {
             try
@@ -115,9 +114,7 @@ namespace CRM_Auto.Controllers
             }
         }
 
-=======
         [HttpGet]
->>>>>>> c71f3f1f359cab2e128c869cde824e50a3560a43
         public IActionResult BuscarFuncionariosEOficinas()
         {
             try
@@ -135,34 +132,7 @@ namespace CRM_Auto.Controllers
             {
                 throw new Exception(ex.Message);
             }
-        }
-
-<<<<<<< HEAD
-        public IActionResult ListarClientes()
-        {
-            try
-            {
-                ClienteModel cliente = new ClienteModel();
-                List<ClienteModel> lista = cliente.ListarClientes();
-                ViewBag.ListarClientes = cliente.ListarClientes();
-=======
-        [HttpGet]
-        public IActionResult BuscarOficinas()
-        {
-            try
-            {
-
-                OficinaModel oficina = new OficinaModel();
-                List<OficinaModel> lista = oficina.BuscarOficinas();
-                ViewBag.BuscarOficinas = oficina.BuscarOficinas();
-
-                return View("CadastroDeOficina");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+        }        
 
         [HttpPost]
         public IActionResult AlterarFuncionario(FuncionarioModel funcionario)
@@ -184,15 +154,14 @@ namespace CRM_Auto.Controllers
         [HttpPost]
         public IActionResult AlterarOficina(OficinaModel oficina)
         {
-            oficina.AlterarOficina(oficina);
+            try
+            {
+                oficina.AlterarOficina(oficina);
 
-            TempData["msg"] = "Alteração realizada com sucesso!";
-            TempData["msgDetalhes"] = "A alteração da oficina foi finalizada e você já pode consultar as informações atualizadas no sistema da sua oficina.";
+                TempData["msg"] = "Alteração realizada com sucesso!";
+                TempData["msgDetalhes"] = "A alteração da oficina foi finalizada e você já pode consultar as informações atualizadas no sistema da sua oficina.";
 
-            return View("CadastroRealizadoComSucesso");
->>>>>>> c71f3f1f359cab2e128c869cde824e50a3560a43
-
-                return View("ListaCliente");
+                return View("CadastroRealizadoComSucesso");
             }
             catch (Exception ex)
             {
--- src/CRM Auto/CRM Auto/Models/ArrayList.cs
+++ src/CRM Auto/CRM Auto/Models/ArrayList.cs
@@ -5,11 +5,6 @@ namespace CRM_Auto.Models
 {
     internal class ArrayList<T> : List<FuncionarioModel>
     {
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> c71f3f1f359cab2e128c869cde824e50a3560a43
     }
 
     internal class ArrayListOficina<T> : List<OficinaModel>
@@ -19,12 +14,9 @@ namespace CRM_Auto.Models
     internal class ArrayListServico<T> : List<ServicoModel>
     {
     }
-<<<<<<< HEAD
-
+    
     internal class ArrayListCliente<T> : List<ClienteModel>
     {
         public List<ClienteModel> Cliente { get; set; }
     }
-=======
->>>>>>> c71f3f1f359cab2e128c869cde824e50a3560a43
 }
